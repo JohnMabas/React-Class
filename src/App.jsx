5c3acf2873bc9e5c89c7b0c8fd610521bd6d0975
mabas <johnmabas1@gmail.com>
2026-08-05 10:30:30 +0100

    learning React

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+
+function Nav(){
+  return(
+    <>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Blog</li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
