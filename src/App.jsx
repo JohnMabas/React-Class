0225505a125b2d6688422ebd90524f72ddd3aac0
mabas <johnmabas1@gmail.com>
2026-08-08 21:39:17 +0100

    add a wed 3 descrition

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Hero />
       <div className="flex justify-center items-center gap-3">
         <Platform Track="Beginner" image={img} Descrition="loprhrhvbgiebvib" />
-        <Platform Track="Wed 3" image={img} Descrition="loprhrhvbgiebvib" />
+        <Platform Track="Wed 3" image={img} Descrition="Welocome to Wed 3" />
         <Platform Track="Advanced" image={img} Descrition="Welcome to Advanced" />
       </div>
 
