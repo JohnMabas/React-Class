aa40f7d7e65d07bc40c73914dc74fbcc40f0ed20
mabas <johnmabas1@gmail.com>
2026-08-10 10:14:29 +0100

    add aboutCard function

---
+++ src/AboutCards.jsx
@@ -0,0 +1,33 @@
+function AboutCards() {
+    const card = [
+        {
+            id: 1,
+            name: "Rolex",
+            price: 200,
+            discription: "the last Rolex available",
+        },
+         {
+            id: 2,
+            name: "Hublot",
+            price: 200,
+            discription: "the last Hublot available",
+        },
+         {
+            id: 3,
+            name: "Rolex",
+            price: 200,
+            discription: "the last Rolex available",
+        },
+    ];
+
+    return(
+        <>
+        
+        </>
+    )
+
+
+}
+
+export default AboutCards;
+
