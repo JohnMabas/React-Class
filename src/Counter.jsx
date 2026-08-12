57fe3d3e0fa693192c3c948a6096c4f5822c4b15
mabas <johnmabas1@gmail.com>
2026-08-12 14:55:52 +0100

    update

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,32 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    function increase () {
-        setCount(count + 1)
-        
-    }
+  function increase() {
+    setCount(count + 1);
+  }
 
-    function decrease () {
-        setCount(count - 1)
-        
-    }
+  function decrease() {
+    setCount(count - 1);
+  }
 
-    function reset () {
-        setCount(0)
-        
-    }
+  function reset() {
+    setCount(0);
+  }
   return (
     <div>
-        <p>{count}</p>
-        <button onClick={increase}>Increase </button>
-        <button onClick={decrease}>Decrease </button>
-        <button onClick={reset}>Reset </button>
-
-      
+      <p>{count}</p>
+      <button onClick={increase}>Increase </button>
+      <button onClick={decrease}>Decrease </button>
+      <button onClick={reset}>Reset </button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
