c1f17a5418b1d6d74551b3041b618865528da2f1
mabas <johnmabas1@gmail.com>
2026-08-14 15:35:41 +0100

    fetch Date

---
+++ src/FetchData.jsx
@@ -0,0 +1,33 @@
+
+import { useState, useEffect } from "react"
+
+const FetchData = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        async function getUsers() {
+            try {
+                const res = await fetch('https://dummyjson.com/users')
+                const data = await res.json()
+                setUsers(data.users)
+                console.log(data)
+            }catch (error) {
+                console.log(error.message)
+            }
+            
+        }
+        getUsers()
+    },[])
+  return (
+    <>
+   
+    {
+       
+
+        })
+    }
+    </>
+  )
+}
+
+export default FetchData
